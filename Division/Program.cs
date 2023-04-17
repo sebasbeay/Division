f8f4f7685c9fff603223b7c3a15e86f863aef46b
Sebastian Bernal <sebasbeay25@gmail.com>
2023-04-16 21:16:11 -0500

    Agregar archivos de proyecto.

---
+++ Division/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+int Division (int numero1, int numero2)
+{
+    return numero1 / numero2;
+}
+
+
+Console.WriteLine(Division(10, 2));
